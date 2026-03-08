fbc54e1bbfa2c2680bf777f41aa8aafbbf5d8b35
= <=>
2026-03-09 00:51:16 +0700

    level controller

--- POS/routes/web.php
+++ POS/routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\SalesController;
+use App\Http\Controllers\LevelController;
 
 /*
 |--------------------------------------------------------------------------
@@ -29,3 +30,5 @@ Route::prefix('category')->group(function(){
 Route::get('/user/{id}/name/{name}', [UserController::class,'profile']);
 
 Route::get('/sales', [SalesController::class,'index']);
+
+Route::get('/level', [LevelController::class,'index']);
