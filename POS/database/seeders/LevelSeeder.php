17148a64569904f40edf94320e7b7bb616216261
= <=>
2026-03-09 00:25:43 +0700

    seeder

---
+++ POS/database/seeders/LevelSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class LevelSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('m_level')->insert([
+        [
+        'level_kode'=>'ADM',
+        'level_nama'=>'Administrator'
+        ],
+        [
+        'level_kode'=>'KSR',
+        'level_nama'=>'Kasir'
+        ],
+        [
+        'level_kode'=>'MNG',
+        'level_nama'=>'Manager'
+        ]
+        ]);
+    }
+}
